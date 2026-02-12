1abcf108667cae58e78130e2ebf76e74fc8703d7
ezmiller-su <166886827+ezmiller-su@users.noreply.github.com>
2026-02-12 09:23:04 -0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import json
+import random
+
+st.set_page_config(page_title="Is This Something?", page_icon=None, layout="wide", menu_items=None)
+
+with open('seinfeld_bits.json', 'r') as file:
+    data = json.load(file)
+
+n = sum(1 for line in data)-1
+
+x = random.randrange(0,n)
+
+st.title(data[x]["title"])
+
+st.text(data[x]["body"])
